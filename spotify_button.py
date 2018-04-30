a8115e9e441fb6e241bf04ccd649d92485ad4774
Brendan White <bmwt@ghostplanet.org>
2018-04-30 00:15:52 +0000

    spotify!

---
+++ spotify_button.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import aiy.voicehat
+import aiy.audio
+import spotipy
+import spotipy.util as util
+from metadata import username, client_id, client_secret
+scope = 'user-modify-playback-state user-read-playback-state user-read-recently-played'
+redirect_uri = 'http://localhost:8888/callback'
+token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+
+def main():
+  button = aiy.voicehat.get_button()
+  while True:
+      print('Press the button to start the party')
+      button.wait_for_press()
+      aiy.audio.say('Starting the jenny dance party', lang="en-US", volume=20, pitch=100)
+      sp = spotipy.Spotify(auth=token)
+      sp.start_playback(device_id="215d73bb194316c27e79eda4ece7385393f324ce", context_uri="spotify:user:bmwt:playlist:7d3sWbnI45l5nop4LuasRl")
+
+if __name__ == '__main__':
+    main()
+      
